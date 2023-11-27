36873cd4127b96e16b8f328a46a7d2f61e48517c
angelika <dehainaut.angelique@orange.fr>
2023-11-28 00:19:18 +0100

    [switch fetch to axios][FIX][AD]

--- src/components/Product/ShowProduct.jsx
+++ src/components/Product/ShowProduct.jsx
@@ -1,24 +1,17 @@
 import {useState, useEffect} from 'react'
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import product from "../../views/Product/Product.jsx";
 import ImgTest from '../../assets/images/imgTest.png';
+import Axios from "../../services/AxiosService.jsx";
 
 const DetailsProduct = () => {
     const params = useParams();
     const [detailsProducts, setDetailsProducts] = useState([]);
-    const token = localStorage.getItem('token');
-
 
     useEffect(() => {
-        fetch(`http://localhost:8000/api/product/show/${params.id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-            },
-        })
-            .then(res => res.json())
-            .then(json => setDetailsProducts(json))
+        Axios.get(`/product/show/${params.id}`)
+            .then(data =>  setDetailsProducts(data.data)
+            )
     }, []);
 
     return (
@@ -27,7 +20,7 @@ const DetailsProduct = () => {
                 <input className='text-center p-2' type="text" value={detailsProducts.name}/>
                 <input className='text-center p-2' type="text" value={detailsProducts.price}/>
                 <p className='text-center font-bold'>{detailsProducts.stock > 0 ? 'En stock' : 'En rupture'}</p>
-                <button onClick={() => addToCart(product.id)}> Ajouter au panier </button>
+                <button onClick={() => addToCart(product.id)}> Ajouter au panier</button>
             </div>
             <div>
                 <h2>Description du Produit</h2>
