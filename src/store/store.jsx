2bcc8b9925e5c22e6ce88d44b872f1942a2bed0e
angelika <dehainaut.angelique@orange.fr>
2023-11-28 14:52:42 +0100

    [storeCart][FEAT][AD] - add cartStore

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -3,14 +3,24 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import thunk from 'redux-thunk';
+
 const authPersistConfig = {
     key: "auth",
+    storage: storage,
+};
+
+const cartPersistConfig = {
+    key: "cart",
     storage: storage,
 };
 
 const rootReducer = combineReducers({
     auth: persistReducer(
         authPersistConfig, authReducer
+    ),
+
+    cart: persistReducer(
+        cartPersistConfig, authReducer
     )
 });
 export const store = configureStore({
