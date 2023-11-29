35e5193461d5b0026459120ef9262900061e0fae
Notizou <noah.decroix3@gmail.com>
2023-11-29 11:33:18 +0100

    [HomePage][UI][ND] - add carousel

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,4 +1,5 @@
 import homeImage from '/src/assets/images/Home/bg-image.png';
+import {Splide, SplideSlide} from "@splidejs/react-splide";
 const Home = () => {
     return (
         <>
@@ -6,6 +7,67 @@ const Home = () => {
                 <img className={"imgHome absolute w-full h-[40rem] max-h-[40rem]"} src={homeImage} alt=""/>
                 <div className={"homePageTitle absolute bottom-1/2 left-40 text-4xl text-white w-[25rem] h-20 max-h-[20rem]"}>
                     <h1 className={'family leading-[3rem]'}>Mobile Phone: L’innovation mobile révolutionnaire</h1>
+                </div>
+            </div>
+
+            <div className={"w-full mt-4 flex justify-between p-8"}>
+                <div className={'text-center w-1/6'}>
+                    <h1 className={"text-4xl family"}>Téléphone</h1>
+                    <Splide aria-label="My Favorite Images" options={{
+                        autoplay: true,
+                        interval: 4000,
+                        type: 'loop',
+                        arrows: false,
+                    }}>
+                        <SplideSlide className={'flex justify-center my-8'}>
+                            <img src="/src/assets/images/imgTest.png" alt="Image 1"/>
+                        </SplideSlide>
+                        <SplideSlide className={'flex justify-center my-8'}>
+                            <img src="/src/assets/images/imgTest.png" alt="Image 2"/>
+                        </SplideSlide>
+                    </Splide>
+                </div>
+
+                <div className={'text-center w-1/6'}>
+                    <h1 className={"text-4xl family"}>Accessoires</h1>
+                    <Splide aria-label="My Favorite Images" options={{
+                        autoplay: true,
+                        interval: 4000,
+                        type: 'loop',
+                        arrows: false,
+                    }}>
+                        <SplideSlide className={'flex justify-center my-8'}>
+                            <img src="/src/assets/images/imgTest.png" alt="Image 1"/>
+                        </SplideSlide>
+                        <SplideSlide className={'flex justify-center my-8'}>
+                            <img src="/src/assets/images/imgTest.png" alt="Image 2"/>
+                        </SplideSlide>
+                    </Splide>
+                </div>
+
+                <div className={'text-center w-1/6'}>
+                    <h1 className={"text-4xl family"}>Tablettes</h1>
+                    <Splide aria-label="My Favorite Images" options={{
+                        autoplay: true,
+                        interval: 4000,
+                        type: 'loop',
+                        arrows: false,
+                    }}>
+                        <SplideSlide className={'flex justify-center my-8'}>
+                            <img src="/src/assets/images/imgTest.png" alt="Image 1"/>
+                        </SplideSlide>
+                        <SplideSlide className={'flex justify-center my-8'}>
+                            <img src="/src/assets/images/imgTest.png" alt="Image 2"/>
+                        </SplideSlide>
+                    </Splide>
+                </div>
+            </div>
+
+            <div>
+                <h2 className={"text-2xl ml-[5rem] family"}>Nos dernier ajouts: </h2>
+
+                <div>
+
                 </div>
             </div>
         </>
