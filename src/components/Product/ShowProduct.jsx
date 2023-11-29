df7d6f5ab83da3e62fdeea5e5aa7080735d858e2
angelika <dehainaut.angelique@orange.fr>
2023-11-29 21:30:43 +0100

    [Refacto][FEAT][AD] - change showProduct for utilisation component

--- src/components/Product/ShowProduct.jsx
+++ src/components/Product/ShowProduct.jsx
@@ -4,10 +4,11 @@ import ImgTest from '../../assets/images/imgTest.png';
 import Axios from "../../services/AxiosService.jsx";
 import { useSelector } from 'react-redux';
 import {toast} from "react-toastify";
+import ProductImage from "./ProductImage.jsx";
+import product from "../../views/Product/Product.jsx";
 
 const DetailsProduct = () => {
     const isConnected = useSelector((state) => state.auth.isLogged);
-
     const params = useParams();
     const [detailsProducts, setDetailsProducts] = useState([]);
 
@@ -22,8 +23,11 @@ const DetailsProduct = () => {
 
     useEffect(() => {
         Axios.get(`/product/show/${params.id}`)
-            .then(data =>  setDetailsProducts(data.data)
-            )
+            .then(data => {
+                setDetailsProducts(data.data)
+                console.log(detailsProducts.imageProduct)
+            }
+    )
     }, []);
 
     return (
@@ -44,7 +48,8 @@ const DetailsProduct = () => {
             <div className='containerDescriptionProduct'>
                 <h2 className='titleDescriptionProduct'>Description du Produit</h2>
                 <div className='flex my-4 max-sm:block'>
-                    <img className='imageShowProduct' src={ImgTest} alt="image tel"/>
+                    {/*<ProductImage className='imageShowProduct' product={detailsProducts}/>*/}
+                    {/*<img className='imageShowProduct' src={ImgTest} alt="image tel"/>*/}
                     <div className='ml-4 mt-4 family w-2/3'>{detailsProducts.description}</div>
 
                 </div>
