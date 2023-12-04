4c46a9539265cb0fd69da9629af9e529f694de38
sam <coquelet.samuel@gmail.com>
2023-12-04 12:04:31 +0100

    [PAGINATION][FEAT][SC]  add pagination

--- src/components/Utils/pagination.jsx
+++ src/components/Utils/pagination.jsx
@@ -1,1 +1,21 @@
-// todo pagination se matin !!! //
+import React from 'react';
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    const pageNumbers = [...Array(totalPages).keys()].map((num) => num + 1);
+
+    return (
+        <div className="flex justify-center mt-4">
+            {pageNumbers.map((number) => (
+                <button
+                    key={number}
+                    className={`mx-1 px-3 py-2 border rounded ${currentPage === number ? 'bg-blue-500 text-white' : 'bg-white text-blue-500'}`}
+                    onClick={() => onPageChange(number)}
+                >
+                    {number}
+                </button>
+            ))}
+        </div>
+    );
+};
+
+export default Pagination;
--- src/views/Product/Product.jsx
+++ src/views/Product/Product.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CardProduct from '../../components/Product/CardProduct.jsx';
+import Pagination from '../../components/Utils/pagination.jsx';
 
 const Product = () => {
     const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [productsPerPage] = useState(8);
 
     useEffect(() => {
         const fetchProducts = async () => {
             try {
                 const response = await axios.get('http://localhost:8000/api/product/');
-
                 setProducts(response.data);
             } catch (error) {
                 console.error('Erreur lors de la récupération des produits :', error);
@@ -19,6 +21,13 @@ const Product = () => {
         fetchProducts();
     }, []);
 
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
     const addToCart = (productId) => {
         console.log(`Produit ajouté au panier avec l'ID ${productId}`);
     };
@@ -26,10 +35,11 @@ const Product = () => {
     return (
         <div className="w-full p-10">
             <div className="flex flex-wrap justify-start sm:justify-items-start">
-                {products.map((product) => (
-                    <CardProduct key={product.id} product={product} />
+                {currentProducts.map((product) => (
+                    <CardProduct key={product.id} product={product} addToCart={() => addToCart(product.id)} />
                 ))}
             </div>
+            <Pagination currentPage={currentPage} totalPages={Math.ceil(products.length / productsPerPage)} onPageChange={paginate} />
         </div>
     );
 };
